17e868398cbbb10fb44715f716dd731bcc607b15
Qasim Shaikh <qasimsk9764@gmail.com>
2024-05-12 23:05:54 +0530

    second function update

--- stringManipulation.js
+++ stringManipulation.js
@@ -1,3 +1,14 @@
-export function capitalize(sentance){
-    return sentance[0].toUpperCase() + sentance.slice(1);
+//Capitalizing Each Word Function
+
+export function capitalize(capital) {
+    capital = capital.split(" ");
+    for (var i = 0, x = capital.length; i < x; i++) {
+        capital[i] = capital[i][0].toUpperCase() + capital[i].substr(1);
+    }
+
+    return capital.join(" ");
 }
+
+export function capitalizeAll(allcapital){
+    return allcapital.toUpperCase();
+    }
