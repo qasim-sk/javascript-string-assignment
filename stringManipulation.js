2b71efb3366bc2aa4480e5b21e5764fd70ac7879
Qasim Shaikh <qasimsk9764@gmail.com>
2024-05-14 11:22:06 +0530

    word density function added

--- stringManipulation.js
+++ stringManipulation.js
@@ -9,6 +9,45 @@ export function capitalize(capital) {
     return capital.join(" ");
 }
 
-export function capitalizeAll(allcapital){
+//Capitalizing Each Letter Function
+
+export function capitalizeAll(allcapital) {
     return allcapital.toUpperCase();
+}
+
+// Word Existence Check Function
+
+export function isExist(sentance, word) {
+    return sentance.includes(word);
+}
+
+// Word Occurrence Count Function
+
+export function getOccurrenceCount(sentance, word) {
+    const regex = new RegExp(word, "g");
+    const matches = sentance.match(regex);
+    return matches.length;
+}
+
+//Word Density Calculation Function
+
+export function wordDensity(sentance) {
+    const cleanedText = sentance.replace(/[^\w\s]/g, "").toLowerCase();
+    const words = cleanedText.split(/\s+/);
+    const totalWords = words.length;
+    const wordCounts = {};
+
+    words.forEach(word => {
+        wordCounts[word] = (wordCounts[word] || 0) + 1;
+    });
+
+    const wordDensityMap = {};
+
+    for (const word in wordCounts) {
+        const occurrenceCount = wordCounts[word];
+        const density = (occurrenceCount / totalWords) * 100;
+        wordDensityMap[word] = density;
     }
+
+    return wordDensityMap;
+}
